b3b508bdc5288d855464c67db357a582c7e75328
Muhammad Usman <muhammad@weebly.com>
2017-10-17 15:58:18 -0700

    hello darkness my old friend, coming back to fix this now

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,14 +3,8 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 		addLink();
-
-		!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
-		analytics.load("6ZTf0oanOl0OPI8hzXMZw9Ku2WPZg431");
-		analytics.page()
-	}}();
-
 	}
-	}, 10);
+	}, 1);
 	var locationInterval = window.setInterval(checkLocation, 100)
 });
 
@@ -19,23 +13,21 @@ var buttonExists = false;
 var feelingLucky = "https://www.google.com/search?btnI=I%27m+Feeling+Lucky&q="
 
 var addLink = function(){
-	if (window.location.href.indexOf('watch') === -1) {
-		buttonExists = true;
+	console.log('...working');
+	if (window.location.href.indexOf('watch') !== -1) {
+		buttonExists = false;
 		return;
 	}
 	var songTitle = $.trim($('#eow-title[title]').text().replace(/'/g, ""));
 	var songTitle = sanitizeTitle(songTitle);
 	var artistName = '';
-	var button = jQuery(".yt-uix-button-subscription-container");
+	var button = jQuery("#subscribe-button");
 	var link = "https://play.spotify.com/search/" + songTitle;
 	buttonExists = true;
 	var url = 'https://api.spotify.com/v1/search?type=track&query=' + songTitle;
 	url = url.split(' ').join('+');
 	var spotifyResponse;
 	var menuLink = $("#action-panel-overflow-button");
-	var amazonTitle = songTitle.split(' ').join('%20');
-	var amazonLink = "<a class='amazon-link' target='_blank' href='http://smile.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&linkCode=ur2&tag=triangleface-20&linkId=U6QIH3JDQYXL2I7B&keywords="+ amazonTitle +"'><img class='amazon-link-image' src='"+ chrome.extension.getURL("src/images/amazon.png") +"'></a>";
-	// menuLink.parent().parent().after(amazonLink);
 	$.get(url, function(data){
 			spotifyResponse = data;
 			link = data.tracks.items[0].uri;
@@ -51,7 +43,6 @@ var addLink = function(){
 			var homepage = "<a class='homepage-link' target='_blank' href='"+ hLink + "'><img class='homepage-link-image' src='"+ chrome.extension.getURL("src/images/website.png") +"'></a>";
 			var fbLink = "https://www.facebook.com/search/str/"+ artistName +"/keywords_top?ref=usmanity.com";
 			var facebook = "<a class='homepage-link' target='_blank' href='"+ fbLink + "'><img class='fb-link-image' src='"+ chrome.extension.getURL("src/images/fb.png") +"'></a>";
-			// $(".amazon-link").after(homepage).after(facebook);
 	});
 }
 
@@ -65,7 +56,7 @@ var checkLocation = function(){
 			if ($('#progress').length === 0) {
 				currentLocation = window.location.href;
 			}
-		}, 100);
+		}, 10);
 	}
 }
 
